3034002a8165c21fa38fd8f6b6fc04f5a06426cf
Sora <hellxiwd@ya.ru>
2024-03-18 22:45:12 +0500

    Restructured code and finally it's working but it way simple

--- diary_mod.py
+++ diary_mod.py
@@ -1,16 +1,13 @@
-diary_Dict = {}
-class Diary(object):
-    def __init__(self, entry):
-        self.entry = entry
-        entry = input("What are you thinking about? ")
-        diary_Dict[:] = {"Entry":entry}
-    def menu():
-        while True:
-            print("Press 1 to add entry")
-            choose = input("Enter a choose: ")
-            if choose == 1:
-                Diary(object)
-            else:
-                break
-    if __name__ == "__main__":
-       menu()
+class Diary:
+
+    diary_arr = [] 
+
+    def addEntry(self, entry):
+        entry_dict = {entry:'Entry'} #create dictionary
+        self.diary_arr.append(entry_dict)
+
+diary = Diary() #create an instance of diary
+
+entry_add = diary.addEntry('123123')
+print("Test", diary.diary_arr)
+#next step I should add entry by input
