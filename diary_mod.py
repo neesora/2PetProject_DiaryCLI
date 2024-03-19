427b63cb35618e702a83680a07ba01c1f8f509e4
Sora <hellxiwd@ya.ru>
2024-03-19 07:26:28 +0500

    Set up more values into data

--- diary_mod.py
+++ diary_mod.py
@@ -2,12 +2,12 @@ class Diary:
 
     diary_arr = [] 
 
-    def addEntry(self, entry):
-        entry_dict = {entry:'Entry'} #create dictionary
+    def addEntry(self, date, entry, mood):
+        entry_dict = { date:'Date', entry:'Entry', mood:'Mood'} #create dictionary
         self.diary_arr.append(entry_dict)
 
 diary = Diary() #create an instance of diary
 
-entry_add = diary.addEntry('123123')
+entry_add = diary.addEntry('12 march', 'I do anal', 'Fine')
 print("Test", diary.diary_arr)
 #next step I should add entry by input
