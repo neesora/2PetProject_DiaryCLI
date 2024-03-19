421803aa09702d436ac8e99b8ea05cb751843917
Sora <hellxiwd@ya.ru>
2024-03-19 22:07:57 +0500

    commit

--- diary_mod.py
+++ diary_mod.py
@@ -5,6 +5,9 @@ class Diary:
     def addEntry(self, date, entry, mood):
         entry_dict = {date:'Date', entry:'Entry', mood:'Mood'}
         self.diary_arr.append(entry_dict)
+    #def nonStop():
+        #asking entry and mood
+        #call addEntry
 
 diary = Diary() #create an instance of diary
 diary.addEntry(input()) #I must think how to solve it when I try to add
