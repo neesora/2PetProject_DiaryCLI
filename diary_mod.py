23ccb36955bceda46c763a409474143223892e69
Sora <hellxiwd@ya.ru>
2024-03-23 18:09:55 +0500

    I make add new entries in func, but it stack into eternal cycle I don't have any idea why it's isn't working

--- diary_mod.py
+++ diary_mod.py
@@ -1,21 +1,26 @@
 class Diary:
 
     diary_arr = []
-    #num_entries = 0
 
-    def addEntry(self, date, entry, mood):
+    def addEntry(self):
+        date = input("Input the date: ")
+        entry = input("Input some text: ")
+        mood = input("Input the mood: ")
         entry_dict = {date:'Date', entry:'Entry', mood:'Mood'}
         self.diary_arr.append(entry_dict)
-'''    def nonStop():
-        num_entries = int(input("Enter the number of entries: "))
+    def selector(self):
+        print("Choose what do you want. \n 1. Add new entry \n 0. Exit program")
+        choice = input()
         while True:
-            if num_entries == 0:
-                diary.addEntry(date=input("Input the date"), entry=input("Input the text"), mood=input("Input the mood"))
-                num_entries += 1
-            break'''
-        #asking entry and mood
-        #call addEntry
-
+            if choice == "1":
+                diary.addEntry()#stack in eternal cycle
+                #self.selector() re-call func
+            elif choice == "0":
+                break
+            else:
+                print("Choose correct option")
 diary = Diary() #create an instance of diary
-diary.addEntry(date=input("Input the date: "), entry=input("Input the text: "), mood=input("Input the mood: "))
+#diary.addEntry()
+diary.selector()
+#diary.addEntry(date=input("Input the date: "), entry=input("Input the text: "), mood=input("Input the mood: "))
 print("Test", diary.diary_arr)
