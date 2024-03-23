21af7fdccf974b93f3a4e6acfcc59a6e824e9633
Sora <hellxiwd@ya.ru>
2024-03-23 16:36:17 +0500

    Finally I can add entries. Now I should to think how to add multiple entries.

--- diary_mod.py
+++ diary_mod.py
@@ -1,15 +1,21 @@
 class Diary:
 
-    diary_arr = [] 
+    diary_arr = []
+    #num_entries = 0
 
     def addEntry(self, date, entry, mood):
         entry_dict = {date:'Date', entry:'Entry', mood:'Mood'}
         self.diary_arr.append(entry_dict)
-    #def nonStop():
+'''    def nonStop():
+        num_entries = int(input("Enter the number of entries: "))
+        while True:
+            if num_entries == 0:
+                diary.addEntry(date=input("Input the date"), entry=input("Input the text"), mood=input("Input the mood"))
+                num_entries += 1
+            break'''
         #asking entry and mood
         #call addEntry
 
 diary = Diary() #create an instance of diary
-diary.addEntry(input()) #I must think how to solve it when I try to add
-#something this add only "date", but entry and mood not
+diary.addEntry(date=input("Input the date: "), entry=input("Input the text: "), mood=input("Input the mood: "))
 print("Test", diary.diary_arr)
