2143b813f13bd949800cd0e7292d6319dd785051
Sora <hellxiwd@ya.ru>
2024-03-24 14:00:25 +0500

    Append new function for searching entries, but for now I'm stack.

--- diary_mod.py
+++ diary_mod.py
@@ -8,12 +8,22 @@ class Diary:
         mood = input("Input the mood: ")
         entry_dict = {date:'Date', entry:'Entry', mood:'Mood'}
         self.diary_arr.append(entry_dict)
+    def searchEntry(searchKey):
+        searchKey = input("Input date for search entry: ")
+        for searchKey in diary.diary_arr: #seems like wrong searching and doesn't catch the right string
+            if searchKey in diary.diary_arr: #another option I can do through transforming list into dict and tryna find through key
+                findIndex = diary.diary_arr.index(searchKey)
+                print(diary.diary_arr[findIndex])
+            else:
+                print("This date isn't exist")
     def selector(choice):
         while True:
-            print("Choose what do you want. \n 1. Add new entry \n 0. Exit program")
-            choice = int(input())
+            print("Choose what do you want. \n 1. Add new entry \n 2. Search entry by date \n 0. Exit program")
+            choice = int(input()) #for values in there updating choice should be in while loop.
             if choice == 1:
                 diary.addEntry()
+            elif choice == 2:
+                diary.searchEntry()
             elif choice == 0:
                 break
             else:
