0d506ce91ba6fac9f31b78d13a660d016ac13fcf
Sora <hellxiwd@ya.ru>
2024-04-02 21:49:52 +0500

    In progress...

--- diary_mod.py
+++ diary_mod.py
@@ -24,22 +24,20 @@ class Diary:
 
     def searchEntry(self):
         searchKey = input("Input date for search entry: ")
-        for entry in self.diary_arr: #that's brute entries
-            if searchKey in entry:
-                print(entry)
-                break
-            else:
-                print("This entry doesn't found")
+        conc = cur.execute('SELECT ? FROM Entries;', searchKey)
+        cur.fetchall()
+        print(conc)
 
     def modEntry(self):
         searchKey = input("Input date for search mod entry: ")
-        for ind, entryStr in enumerate(self.diary_arr):
-            if searchKey in entryStr:
+        Entries = cur.fetchall()
+        for entryX in Entries:
+            if searchKey in entryX:
                 date = input("Input new date: ")
                 entry = input("Input new entry: ")
                 mood = input("Input new mood: ")
-                entry_dict = {date:"Date", entry:"Entry", mood:"Mood"}
-                self.diary_arr[ind] = entry_dict
+                cur.execute('UPDATE Entries SET (date = ?, entry = ?, mood = ?) WHERE searchKey;', (date, entry, mood))
+                con.commit()
                 break
             else:
                 print("This entry doesn't found")
@@ -69,7 +67,8 @@ class Diary:
                 case 4:
                     diary.delEntry()
                 case 0:
-                    diary.sqlite()
+                    con.commit()
+                    con.close()
                     break
                 case _:
                     print("Choose correct option")
